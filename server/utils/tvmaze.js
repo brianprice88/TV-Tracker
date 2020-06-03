5504bbc30521179fca98baeb0a03d06c3c95db51
Brian Price <brianprice88@gmail.com>
2020-06-03 14:44:14 -0400

    "finish tvmaze api helpers.  Need to integrate them into server routes and test."

--- server/utils/tvmaze.js
+++ server/utils/tvmaze.js
@@ -4,21 +4,23 @@ const url = 'http://api.tvmaze.com';
 const apiRequests = {
 
     getDailySchedule: (year, month, day) => {
-        let endpoint = `${url}/schedule?country=US&date=${year}-${month}-${day}`
+        let endpoint = `${url}/schedule?country=US&date=${year}-${month}-${day}` // only for US schedule (for now, at least)
         axios.get(endpoint)
             .then(function (response) {
                 let data = response.data;
                 let shows = [];
                 for (var i = 0; i < data.length; i++) {
-                  let show = data[i];
-                  let showInfo = {};
-                  showInfo.showtvmazeId = show.show.id;
-                  showInfo.showName = show.show.name;
-                  showInfo.episodeName = show.name;
-                  showInfo.season = show.season;
-                  showInfo.number = show.number;
-                  showInfo.time = show.airtime;
-                  showInfo.summary = show.summary;
+                    let show = data[i];
+                    let showInfo = {};
+                    showInfo.showtvmazeId = show.show.id;
+                    showInfo.showName = show.show.name;
+                    showInfo.episodeName = show.name;
+                    showInfo.season = show.season;
+                    showInfo.number = show.number;
+                    showInfo.time = show.airtime;
+                    showInfo.summary = show.summary;
+                    showInfo.network = show.show.network.name;
+                    shows.push(showInfo)
                 }
                 return shows;
             })
@@ -30,7 +32,18 @@ const apiRequests = {
         axios.get(endpoint)
             .then(function (response) {
                 let data = response.data;
-                return data;
+                let availShows = data.filter(show => show.show.summary !== null) // includes popular non-English language shows like on Netflix
+                let shows = [];
+                for (var i = 0; i < availShows.length; i++) {
+                    let show = availShows[i];
+                    let showInfo = {};
+                    showInfo.tvmazeId = show.id;
+                    showInfo.name = show.name;
+                    showInfo.network = show.network.name;
+                    showInfo.summary = show.summary;
+                    shows.push(showInfo)
+                }
+                return shows;
             }).catch(err => console.error(err))
     },
 
@@ -39,7 +52,15 @@ const apiRequests = {
         axios.get(endpoint)
             .then(function (response) {
                 let data = response.data;
-                return data;
+                let episodes = [];
+                for (var i = 0; i < data.length; i++) {
+                    let episode = data[i];
+                    let episodeInfo = {};
+                    episodeInfo.season = episode.season;
+                    episodeInfo.number = episode.number;
+                    episodes.push(episodeInfo)
+                }
+                return episodes;
             }).catch(err => console.error(err))
     }
 
