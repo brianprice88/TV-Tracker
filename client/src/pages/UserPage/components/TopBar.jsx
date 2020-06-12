c0711ec78089e9faeebac04c4b335ae9d50edd6c
Brian Price <brianprice88@gmail.com>
2020-06-12 16:46:29 -0400

    "send feedback request working"

--- client/src/pages/UserPage/components/TopBar.jsx
+++ client/src/pages/UserPage/components/TopBar.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './TopBar.css'
 
 export default function TopBar({ axiosHandler, user }) {
@@ -25,92 +25,104 @@ export default function TopBar({ axiosHandler, user }) {
                         </div>
                         <div class="dropdown-menu">
 
-                            <div className='nav-link navBarButton' onClick = {() => showForm('UpdateEmail')}>Update Email</div>
-    
-                            <div class="dropdown-divider"></div>
-
-                            <div className='nav-link navBarButton' onClick = {() => showForm('UpdatePassword')}>Update Password</div>
+                            <div className='nav-link navBarButton' onClick={() => showForm('UpdateEmail')}>Update Email</div>
 
                             <div class="dropdown-divider"></div>
 
-                            <div className='nav-link navBarButton' onClick = {() => showForm('SendFeedback')}>Send Feedback</div>
+                            <div className='nav-link navBarButton' onClick={() => showForm('UpdatePassword')}>Update Password</div>
 
                             <div class="dropdown-divider"></div>
 
-                            <div className='nav-link navBarButton' onClick = {() => showForm('DeleteAccount')}>Delete Account</div>
+                            <div className='nav-link navBarButton' onClick={() => showForm('SendFeedback')}>Send Feedback</div>
+
+                            <div class="dropdown-divider"></div>
+
+                            <div className='nav-link navBarButton' onClick={() => showForm('DeleteAccount')}>Delete Account</div>
 
                         </div>
                     </li>
                 </ul>
+
+
+                {formType === 'UpdateEmail'
+                    ? <UpdateEmail user={user}
+                        axiosHandler={axiosHandler}
+                        showForm={showForm} />
+                    : null}
+
+                {formType === 'UpdatePassword'
+                    ? <UpdatePassword user={user}
+                        axiosHandler={axiosHandler}
+                        showForm={showForm} />
+                    : null}
+
+                {formType === 'SendFeedback'
+                    ? <SendFeedback user={user}
+                        axiosHandler={axiosHandler}
+                        showForm={showForm} />
+                    : null}
+
+                {formType === 'DeleteAccount'
+                    ? <DeleteAccount user={user}
+                        axiosHandler={axiosHandler}
+                        showForm={showForm} />
+                    : null}
+
             </div>
 
-            {formType === 'UpdateEmail' 
-                            ? <UpdateEmail user = {user}
-                            axiosHandler = {axiosHandler}
-                            showForm = {showForm}/>
-                            : null}
-
-{formType === 'UpdatePassword' 
-                            ? <UpdatePassword user = {user}
-                            axiosHandler = {axiosHandler}
-                            showForm = {showForm}/>
-                            : null}
-
-{formType === 'SendFeedback' 
-                            ? <SendFeedback user = {user}
-                            axiosHandler = {axiosHandler}
-                            showForm = {showForm}/>
-                            : null}
-
-{formType === 'DeleteAccount' 
-                            ? <DeleteAccount user = {user}
-                            axiosHandler = {axiosHandler}
-                            showForm = {showForm}/>
-                            : null}
-           
         </nav>
     )
 }
 
 
 
-function UpdateEmail ({user, axiosHandler, showForm}) {
+function UpdateEmail({ user, axiosHandler, showForm }) {
     return (
         <form>
             <h2>Update your email</h2>
-        <button onClick={() => axiosHandler('deleteAccount', user.email_address, user.session)} type="button" class="btn btn-danger" data-dismiss="modal">Confirm</button>
-        <button onClick={() => showForm(null)} type="button" class="btn btn-danger" data-dismiss="modal">Back</button>
-            </form>
+            <button onClick={() => axiosHandler('deleteAccount', user.email_address, user.session)} type="button" class="btn btn-danger" data-dismiss="modal">Confirm</button>
+            <button onClick={() => showForm(null)} type="button" class="btn btn-danger" data-dismiss="modal">Back</button>
+        </form>
     )
 };
 
-function UpdatePassword ({user, axiosHandler, showForm}) {
+function UpdatePassword({ user, axiosHandler, showForm }) {
     return (
         <form>
             <h2>Update your password</h2>
-        <button onClick={() => axiosHandler('deleteAccount', user.email_address, user.session)} type="button" class="btn btn-danger" data-dismiss="modal">Confirm</button>
-        <button onClick={() => showForm(null)} type="button" class="btn btn-danger" data-dismiss="modal">Back</button>
-            </form>
+            <button onClick={() => axiosHandler('deleteAccount', user.email_address, user.session)} type="button" class="btn btn-danger" data-dismiss="modal">Confirm</button>
+            <button onClick={() => showForm(null)} type="button" class="btn btn-danger" data-dismiss="modal">Back</button>
+        </form>
     )
 };
 
-function SendFeedback ({user, axiosHandler, showForm}) {
+function SendFeedback({ user, axiosHandler, showForm }) {
+    function sendUserFeedback(e) {
+        e.preventDefault();
+        let feedback = document.getElementById('userMessage').value;
+        let message = feedback.replace(/[^a-z0-9]/gi, '');
+        document.getElementById('userMessage').value = '';
+        axiosHandler('sendFeedback', user.email_address, user.session, message)
+    }
+
     return (
-        <form>
-            <h2>Send your message</h2>
-        <button onClick={() => axiosHandler('deleteAccount', user.email_address, user.session)} type="button" class="btn btn-danger" data-dismiss="modal">Confirm</button>
-        <button onClick={() => showForm(null)} type="button" class="btn btn-danger" data-dismiss="modal">Back</button>
+        <div className="form-group">
+            <form onSubmit={sendUserFeedback}>
+            <textarea placeholder='Enter your questions or comments here' className="form-control" id="userMessage" rows="3"></textarea>
+            <button type="submit" class="btn btn-primary mb-2">Send message</button>
+            <button onClick={() => showForm(null)} type="button" class="btn btn-danger" data-dismiss="modal">Back</button>
             </form>
+        </div>
     )
 };
 
 
-function DeleteAccount ({user, axiosHandler, showForm}) {
-    return (     
-            <form>
+function DeleteAccount({ user, axiosHandler, showForm }) {
+    return (
+        <div className='form-group'>
             <h2>Warning!  This will erase all your progress</h2>
-        <button onClick={() => axiosHandler('deleteAccount', user.email_address, user.session)} type="button" class="btn btn-danger" data-dismiss="modal">Confirm</button>
-        <button onClick={() => showForm(null)} type="button" class="btn btn-danger" data-dismiss="modal">Exit</button>
-            </form>
+            <button onClick={() => axiosHandler('deleteAccount', user.email_address, user.session)} type="button" class="btn btn-danger" data-dismiss="modal">Confirm</button>
+            <button onClick={() => showForm(null)} type="button" class="btn btn-danger" data-dismiss="modal">Exit</button>
+        </div>
     )
 }
