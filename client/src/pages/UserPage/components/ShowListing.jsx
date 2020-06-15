15ee80f7e1fa3db642aef0500f675a55566ad055
Brian Price <brianprice88@gmail.com>
2020-06-15 01:22:48 -0400

    "fix issue with notification toggler state"

--- client/src/pages/UserPage/components/ShowListing.jsx
+++ client/src/pages/UserPage/components/ShowListing.jsx
@@ -1,39 +1,48 @@
 import React from 'react';
 import './ShowListing.css'
-import { toggleNotification } from '../../../utils/axiosFunctions';
+import Episodes from './Episodes'
 
 export default function ShowList({ show, axiosHandler, user }) {
     let name = show[0];
     let { tvmaze_id, notification, episodes } = show[1];
-    let {email_address, session} = user;
+    let { email_address, session } = user;
 
     function removeShow() {
-        axiosHandler('removeShow', email_address, session, tvmaze_id) 
+        axiosHandler('removeShow', email_address, session, tvmaze_id)
     }
 
-    function toggleShowNotification() {
-    axiosHandler('toggleNotification', email_address, session, tvmaze_id)   
+    function toggleShowNotification(e) {
+        axiosHandler('toggleNotification', email_address, session, tvmaze_id);
     }
 
     return (
         <tr>
             <td>
                 {name}
-                <button onClick = {removeShow} type="button" className="btn btn-warning">Remove from list</button>
+                <button onClick={removeShow} type="button" className="btn btn-warning">Remove from list</button>
             </td>
 
             <td>
-
                 <div className="flipswitch">
-                    <input onClick={toggleShowNotification} type="checkbox" name="flipswitch" className="flipswitch-cb" id={`${tvmaze_id}`} value={notification ? 'on' : 'off'} />
-                    <label className="flipswitch-label" htmlFor={`${tvmaze_id}`}>
+                    <input onClick={(e) => toggleShowNotification(e)} type="checkbox" name="flipswitch" className="flipswitch-cb" id={`${tvmaze_id}-toggle`}  defaultChecked={notification ? 'checked' : ''}/>
+                    <label className="flipswitch-label" htmlFor={`${tvmaze_id}-toggle`}>
                         <div className="flipswitch-inner"></div>
                         <div className="flipswitch-switch"></div>
                     </label>
                 </div>
             </td>
 
-            <td>Episodes</td>
+            <td>
+                <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#myModal">
+                    View your progress
+                </button>
+                <Episodes 
+                axiosHandler={axiosHandler}
+                show={show}
+                user={user}
+                />
+            </td>
+
         </tr>
     )
 }
