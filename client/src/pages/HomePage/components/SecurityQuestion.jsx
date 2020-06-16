8e6b5bc62632e0ee7e7185d781361a62ae866c51
Brian Price <brianprice88@gmail.com>
2020-06-16 14:18:44 -0400

    "add styling to security question.  moving on to style user page"

--- client/src/pages/HomePage/components/SecurityQuestion.jsx
+++ client/src/pages/HomePage/components/SecurityQuestion.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Alert from '../../App/components/Alert';
+import './SecurityQuestion.css'
 import { getSecurityQuestion, checkSecurityAnswer, resetPassword } from '../../../utils/axiosFunctions'
 
 
@@ -40,50 +41,54 @@ export default function SecurityQuestion({ showSecurityQuestion }) {
     }
 
     return (
-        <form onSubmit={(e) => updatePassword(e)} id='forgotPasswordForm'>
+        <div className='container-fluid'>
+            <div className='row justify-content-center'>
+                <form onSubmit={(e) => updatePassword(e)} id='forgotPasswordForm'>
 
-            {alert ?
-                <Alert
-                    clearAlert={() => updateAlert(null)}
-                    alert={alert}
-                />
-                : null
-            }
+                    {alert ?
+                        <Alert
+                            clearAlert={() => updateAlert(null)}
+                            alert={alert}
+                        />
+                        : null
+                    }
 
-            <button onClick={() => { showSecurityQuestion(false) }} type="button" className="close" >Go Back</button>
-            <h1>Security Verification</h1>
+                    <button onClick={() => { showSecurityQuestion(false) }} type="button" className="close" >Go Back</button>
+                    <h1>Verification</h1>
 
-            <div className="input-group mb-3">
-                <input id='secQuestionEmail' type="email" className="form-control" placeholder="Your Email Address" autoComplete="on" required />
-                <div className="input-group-append">
-                    <button className="btn btn-secondary" type="button" onClick={queryForSecurityQuestion}>Get Security Question</button>
-                </div>
-            </div>
-
-            {prompt ?
-                <div className="input-group mb-3 input-lg">
-                    <input id='secQuestionAnswer' type="password" className="form-control" placeholder={prompt} autoComplete="on" required />
-                    <div className="input-group-append">
-                        <button className="btn btn-secondary" type="button" onClick={verifySecurityAnswer}>Submit</button>
-                    </div>
-                </div>
-                :
-                null
-            }
-
-            {prompt === 'Please update your password.' ?
-                    <div className="input-group mb-3 input-lg">
-                        <input id='resetPassword' type="password" className="form-control" placeholder={prompt} autoComplete="on" required
-                            pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,20}"
-                            title="Must contain at least one number and one uppercase and lowercase letter, and 8-20 characters" />
+                    <div className="input-group mb-3">
+                        <input id='secQuestionEmail' type="email" className="form-control" placeholder="Enter Email Address" autoComplete="on" required />
                         <div className="input-group-append">
-                            <button className="btn btn-secondary" type="submit">Update password</button>
+                            <button className="btn btn-secondary" type="button" onClick={queryForSecurityQuestion}>Get Security Question</button>
                         </div>
                     </div>
-                    :
-                    null
-            }
-        </form>
+
+                    {prompt ?
+                        <div className="input-group mb-3 input-lg">
+                            <input id='secQuestionAnswer' type="password" className="form-control" placeholder={prompt} autoComplete="on" required />
+                            <div className="input-group-append">
+                                <button className="btn btn-secondary" type="button" onClick={verifySecurityAnswer}>Check Answer</button>
+                            </div>
+                        </div>
+                        :
+                        null
+                    }
+
+                    {prompt === 'Please update your password.' ?
+                        <div className="input-group mb-3 input-lg">
+                            <input id='resetPassword' type="password" className="form-control" placeholder={prompt} autoComplete="on" required
+                                pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,20}"
+                                title="Must contain at least one number and one uppercase and lowercase letter, and 8-20 characters" />
+                            <div className="input-group-append">
+                                <button className="btn btn-secondary" type="submit">Update password</button>
+                            </div>
+                        </div>
+                        :
+                        null
+                    }
+                </form>
+            </div>
+        </div>
     )
 
 }
