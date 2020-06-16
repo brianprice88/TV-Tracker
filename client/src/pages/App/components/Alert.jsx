de1d86abc66fc8901d6f791695996ecd43186e7a
Brian Price <brianprice88@gmail.com>
2020-06-16 01:17:23 -0400

    "create separate alert component"

---
+++ client/src/pages/App/components/Alert.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Alert.css'
+
+export default function Alert({ clearAlert, alert }) {
+    return (
+
+        <div className='popup'>
+            <h4>ALERT!</h4>
+            <div className='popup\_inner'>
+                <p>{alert}</p>
+                <button className= "btn btn-light" onClick={clearAlert}>Dismiss</button>
+            </div>
+        </div>
+    )
+}
