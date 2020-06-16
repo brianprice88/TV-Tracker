a18fa6571d7952062bcfeaab715751dc9f0b6fc3
Brian Price <brianprice88@gmail.com>
2020-06-16 15:49:28 -0400

    "styled topbar component to look much better"

--- client/src/pages/UserPage/components/TopBar.jsx
+++ client/src/pages/UserPage/components/TopBar.jsx
@@ -11,7 +11,7 @@ export default function TopBar({ axiosHandler, user }) {
             <h1>Welcome {user.email_address}!</h1>
         </div>
 
-        <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top ">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
                 <span className="navbar-toggler-icon"></span>
             </button>
@@ -19,27 +19,37 @@ export default function TopBar({ axiosHandler, user }) {
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav">
                     <li className="nav-item">
-                        <div className='nav-link navBarButton' onClick={() => axiosHandler('signOut', user.email_address, user.session)}>Sign Out</div>
+                        <div className='nav-link navBarButton' onClick={() => axiosHandler('signOut', user.email_address, user.session)}>
+                            <span>Sign Out</span>
+                            </div>
                     </li>
                     <li className="nav-item dropdown">
                         <div className="nav-link dropdown-toggle navBarButton" role="button" data-toggle="dropdown">
-                            Options
+                            <span>Options</span>
                         </div>
-                        <div className="dropdown-menu">
+                        <div className="dropdown-menu dropdownOptions">
 
-                            <div className='nav-link navBarButton' onClick={() => showForm('UpdateEmail')}>Update Email</div>
-
-                            <div className="dropdown-divider"></div>
-
-                            <div className='nav-link navBarButton' onClick={() => showForm('UpdatePassword')}>Update Password</div>
+                            <div className='nav-link navBarButton' onClick={() => showForm('UpdateEmail')}>
+                                <span>Update Email</span>
+                                </div>
 
                             <div className="dropdown-divider"></div>
 
-                            <div className='nav-link navBarButton' onClick={() => showForm('SendFeedback')}>Send Feedback</div>
+                            <div className='nav-link navBarButton' onClick={() => showForm('UpdatePassword')}>
+                                <span>Update Password</span>
+                                </div>
 
                             <div className="dropdown-divider"></div>
 
-                            <div className='nav-link navBarButton' onClick={() => showForm('DeleteAccount')}>Delete Account</div>
+                            <div className='nav-link navBarButton' onClick={() => showForm('SendFeedback')}>
+                                <span>Send Feedback</span>
+                                </div>
+
+                            <div className="dropdown-divider"></div>
+
+                            <div className='nav-link navBarButton' onClick={() => showForm('DeleteAccount')}>
+                                <span>Delete Account</span>
+                                </div>
 
                         </div>
                     </li>
@@ -83,17 +93,22 @@ function UpdateEmail({ user, axiosHandler, showForm }) {
         e.preventDefault();
         let newEmail = document.getElementById('updatedEmail').value
         document.getElementById('updatedEmail').value = '';
-        axiosHandler('updateInfo', user.email_address, user.session, 'email', newEmail)
+        axiosHandler('updateInfo', user.email_address, user.session, 'email', newEmail);
+        showForm(null)
     }
     return (
         <form onSubmit={changeEmail}>
-            <div className="form-group">
-                <label>New Email:</label>
-                <input id='updatedEmail' type="email" className="form-control" placeholder="YourEmail@domain.com" name="email_address" required />
+            <div className="form-row align-items-center">
+            <div className="col-auto">
+                <input id='updatedEmail' type="email" className="form-control mb-2" placeholder="New Email" name="email_address" required />
             </div>
+            <div className='col-auto'>
             <button type="submit" className="btn btn-primary mb-2">Update Email</button>
-
-            <button onClick={() => showForm(null)} type="button" className="btn btn-danger" data-dismiss="modal">Back</button>
+            </div>
+            <div className='col-auto'>
+            <button onClick={() => showForm(null)} type="button" className="btn btn-danger mb-2" data-dismiss="modal">Back</button>
+            </div>
+            </div>
         </form>
     )
 };
@@ -104,20 +119,26 @@ function UpdatePassword({ user, axiosHandler, showForm }) {
         let newPassword = document.getElementById('updatedPassword').value;
         document.getElementById('updatedPassword').value = '';
         axiosHandler('updateInfo', user.email_address, user.session, 'password', newPassword);
+        showForm(null)
     }
     return (
-        <div className='form-group'>
             <form onSubmit={changePassword}>
-                <label>New Password:</label>
-                <input id='updatedPassword' type="password" className="form-control"
-                    placeholder="Must contain a number, uppercase and lowercase letter"
+             <div className="form-row align-items-center">
+            <div className="col-auto">
+                <input id='updatedPassword' type="password" className="form-control mb-2"
+                    placeholder="New Password"
                     name="password" required
                     pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,20}"
                     title="Must contain at least one number and one uppercase and lowercase letter, and 8-20 characters" />
+                </div>
+                <div className='col-auto'>
                 <button type="submit" className="btn btn-primary mb-2">Update Password</button>
-                <button onClick={() => showForm(null)} type="button" className="btn btn-danger" data-dismiss="modal">Back</button>
+                </div>
+                <div className='col-auto'>
+                <button onClick={() => showForm(null)} type="button" className="btn btn-danger mb-2" data-dismiss="modal">Back</button>
+            </div>
+            </div>
             </form>
-        </div>
     )
 };
 
@@ -127,27 +148,39 @@ function SendFeedback({ user, axiosHandler, showForm }) {
         let feedback = document.getElementById('userMessage').value;
         let message = feedback.replace(/[^a-z0-9]/gi, '');
         document.getElementById('userMessage').value = '';
-        axiosHandler('sendFeedback', user.email_address, user.session, message)
+        axiosHandler('sendFeedback', user.email_address, user.session, message);
+        showForm(null)
     }
-
     return (
-        <div className="form-group">
             <form onSubmit={sendUserFeedback}>
-                <textarea placeholder='Enter your questions or comments here' className="form-control" id="userMessage" rows="3"></textarea>
-                <button type="submit" className="btn btn-primary mb-2">Send message</button>
-                <button onClick={() => showForm(null)} type="button" className="btn btn-danger" data-dismiss="modal">Back</button>
-            </form>
-        </div>
+                <div className="form-row align-items-center">
+                <div className='col-auto'>
+                    <textarea placeholder='Enter your questions or comments here' className="form-control mb-2" id="userMessage" rows="3"></textarea>
+                    </div>
+                <div className='col-auto'>
+                    <button type="submit" className="btn btn-primary mb-2">Send message</button>
+                    </div>
+                <div className='col-auto'>
+                    <button onClick={() => showForm(null)} type="button" className="btn btn-danger mb-2" data-dismiss="modal">Back</button>
+                    </div>
+           </div>
+           </form>
     )
 };
 
 
 function DeleteAccount({ user, axiosHandler, showForm }) {
     return (
-        <div className='form-group'>
-            <h2>Warning!  This will erase all your progress</h2>
-            <button onClick={() => axiosHandler('deleteAccount', user.email_address, user.session)} type="button" className="btn btn-danger" data-dismiss="modal">Confirm</button>
-            <button onClick={() => showForm(null)} type="button" className="btn btn-danger" data-dismiss="modal">Exit</button>
+        <div className="form-row align-items-center">
+            <div className='col-auto'>
+            <strong>Warning!  This will erase all your progress</strong>
+            </div>
+            <div className='col-auto'>
+            <button onClick={() => axiosHandler('deleteAccount', user.email_address, user.session)} type="button" className="btn btn-primary mb-2" data-dismiss="modal">Confirm</button>
+            </div>
+            <div className='col-auto'>
+            <button onClick={() => showForm(null)} type="button" className="btn btn-danger mb-2" data-dismiss="modal">Back</button>
+            </div>
         </div>
     )
 }
