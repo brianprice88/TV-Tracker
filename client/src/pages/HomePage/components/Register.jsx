3820bd85ebfbe0006c15e710c70029fa731457c4
Brian Price <brianprice88@gmail.com>
2020-06-18 14:15:22 -0400

    "add captcha for user signup verification"

--- client/src/pages/HomePage/components/Register.jsx
+++ client/src/pages/HomePage/components/Register.jsx
@@ -1,11 +1,38 @@
-import React, { useReducer } from 'react';
-import './Register.css'
+import React, { useState, useReducer, useEffect } from 'react';
+import './Register.css';
+import Alert from '../../App/components/Alert';
+
 
 export default function Register({ changeFormDisplay, axiosHandler }) {
 
+    const [alert, updateAlert] = useState(null);
+    var code;
+
+    function createCaptcha() {
+        document.getElementById('captcha').innerHTML = "";
+        var chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@!#$%&";
+        var captcha = [];
+        while (captcha.length < 6) {
+            var index = Math.floor(Math.random() * chars.length + 1);
+            if (captcha.indexOf(chars[index]) === -1) {
+                captcha.push(chars[index]);
+            }
+        }
+        var canv = document.createElement("canvas");
+        canv.id = "captcha";
+        canv.width = 150;
+        canv.height = 50;
+        var ctx = canv.getContext("2d");
+        ctx.font = "bold 32px Georgia";
+        ctx.strokeText(captcha.join(""), 0, 30);
+        code = captcha.join("");
+        document.getElementById("captcha").appendChild(canv);
+    }
+
+    useEffect(() => createCaptcha())
 
 
-    const initialFormInfo = { email_address: '', password: '', security_question: 'What was the name of your first pet?', security_answer: '' }
+    const initialFormInfo = { email_address: '', password: '', security_question: 'What was the name of your first pet?', security_answer: '', captcha: '' }
 
     const addNewInfo = function (e) {
         dispatch({ type: e.target.name, payload: e.target.value })
@@ -20,8 +47,13 @@ export default function Register({ changeFormDisplay, axiosHandler }) {
     const [formInfo, dispatch] = useReducer(updateFormInfoState, initialFormInfo)
 
     function handleSubmit(e) {
-        if (!formInfo.security_answer.match(/^[a-zA-Z]+$/)) { return }
         e.preventDefault();
+        if (!formInfo.security_answer.match(/^[a-zA-Z]+$/)) { updateAlert('Your security answer must only contain letters'); return; };
+        let captchaText = formInfo.captcha;
+        if (captchaText !== code) {
+            updateAlert('Incorrect captcha.  Please try again.');
+            return;
+        };
         document.getElementById('registerForm').reset()
         axiosHandler('signUp', formInfo.email_address, formInfo.password, formInfo.security_question, formInfo.security_answer)
     }
@@ -30,6 +62,15 @@ export default function Register({ changeFormDisplay, axiosHandler }) {
         <div className="container-fluid">
             <div className="row justify-content-center">
                 <form id='registerForm' onSubmit={handleSubmit}>
+
+                    {alert ?
+                        <Alert
+                            clearAlert={() => updateAlert(null)}
+                            alert={alert}
+                        />
+                        : null
+                    }
+
                     <button onClick={() => { changeFormDisplay('Home') }} type="button" className="close">Go Back</button>
                     <h1>Sign up</h1>
                     <div className="form-group">
@@ -59,6 +100,12 @@ export default function Register({ changeFormDisplay, axiosHandler }) {
                         <label>Answer for your security question:</label>
                         <input type="password" className="form-control" placeholder="Must only contain letters (not case sensitive)" name="security_answer" required onChange={addNewInfo} />
                     </div>
+                    <div className="form-group">
+                        <label>Prove you're not a robot:</label>
+                        <div id='captcha'></div>
+                        <input type="text" className="form-control" placeholder="Enter the above captcha (case sensitive)" name="captcha" required onChange={addNewInfo} />
+                    </div>
+
                     <button type="submit" className="btn btn-primary">Register!</button>
                 </form>
             </div>
